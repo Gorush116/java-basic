8e34fe57ab4ab0215905a04fbdbf3efe8822ce76
Gorush116 <gorush34@gmail.com>
2024-02-02 23:41:53 +0900

    기본형:변수에 사용할 값 직접 대입/참조형:데이터에 접근하기 위한 참조(주소) 저장

--- src/ref/MethodChange1.java
+++ src/ref/MethodChange1.java
@@ -8,6 +8,8 @@ public class MethodChange1 {
         System.out.println("a = " + a);
         changePrimitive(a);
         System.out.println("a = " + a);
+        // 업무 파악은 머릿 속으로 큰 그림을 먼저 그리고 프로세스를 파악한 뒤 디테일을 챙긴다
+        // 에러 발생시 여러 부분을 확인하여 정확한 원인을 파악하고 외부 API 이용시 공식 문서를 반드시 참고할 것
     }
 
     public static void changePrimitive(int x) {
