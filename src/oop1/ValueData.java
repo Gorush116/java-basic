624d4ef402c3e9ab5159647825acebf8e0b05502
Gorush116 <gorush34@gmail.com>
2024-02-08 09:02:17 +0900

    절차지향 : 어떻게 / 객체지향 : 무엇을

---
+++ src/oop1/ValueData.java
@@ -0,0 +1,16 @@
+package oop1;
+
+public class ValueData {
+
+    /**
+     * 클래스는 속성(데이터, 멤버변수)과 기능(메소드)를 정의할 수 있다.
+     */
+
+    int value;
+
+    void add() {
+        value++;
+        System.out.println("숫자 증가 value = " + value);
+    }
+
+}
